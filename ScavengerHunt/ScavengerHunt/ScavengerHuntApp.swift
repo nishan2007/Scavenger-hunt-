c67340523322657ac08380a9e2eb3eb2def93926
nishan2007 <nishannarain@gmail.com>
2026-02-04 22:08:08 -0500

    Initial commit

---
+++ ScavengerHunt/ScavengerHunt/ScavengerHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScavengerHuntApp.swift
+//  ScavengerHunt
+//
+//  Created by Nishan Narain on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct ScavengerHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
